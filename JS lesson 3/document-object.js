32f0a7c6d6da22ddcd676127e45513b312893705
KienLuongg <mrkiencute@gmail.com>
2022-04-18 21:30:51 +0700

    1

---
+++ JS lesson 3/document-object.js
@@ -0,0 +1,5 @@
+var msg = '<p><b>page title: </b> '+ document.title + '<br />';
+
+msg += '<b>page address: </b>' + document.URL + '</p>';
+
+msg += '<b>last modified: </b>' + document.lastModified + '</p>';
